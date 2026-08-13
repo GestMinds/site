aa82c1016d424bc4de1f0177b4b809739d9e6b74
GestMinds <gestminds@outlook.com>
2026-08-13 19:31:12 -0300

    Pagina refeita

--- app/assets/js/navigation.js
+++
@@ -1,8 +0,0 @@
-async function loadPage(pageName) {
-    const response = await fetch(`pages/${pageName}.html`);
-    const html = await response.text();
-    document.getElementById('main-content').innerHTML = html;
-}
-
-// Ao carregar o app, chama o dashboard por padrão
-loadPage('dashboard');
